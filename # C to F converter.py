7728b72efddf3055bcf54d3a0cf04a31c2c4ced3
sbo97t <63102539+sbo97t@users.noreply.github.com>
2020-04-24 19:32:54 -0500

    Add files via upload

--- # C to F converter.py
+++ # C to F converter.py
@@ -2,9 +2,9 @@
 print("Enter a temp in Celsius to convert to Fahrenheit")
 temp = float(input("Celsius "))
 print((temp * 9/5) + 32)
-print("Would you like to convert from Farhneheit to Celsius? ")
+print("Would you like to convert from Farhneheit to Celsius? (Y or N) ")
 answer=input()
-if answer=="Y" or "y":
+if answer=="Y":
      print("Enter a temp in Fahrenheit to convert to Celsius")
      tempF = float(input("Fahrenheit "))
      print((tempF - 32) * 5/9)
